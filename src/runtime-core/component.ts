4ebdf9d40ebc0f9d610c914ac2fb6b15fabab0c4
elissali <elissali@tencent.com>
2022-03-20 22:13:08 +0800

    runtime-core

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,40 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+  };
+
+  return component;
+}
+
+export function setupComponent(instance) {
+  // initProps()
+  // initSlots()
+  setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance) {
+  const component = instance.vnode.type;
+
+  const { setup } = component;
+
+  if (setup) {
+    const setupResult = setup();
+    handleSetupResult(instance, setupResult);
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+  // function
+  if (typeof setupResult === 'object') {
+    instance.setupState = setupResult;
+    finishComponentSetup(instance);
+  }
+}
+
+function finishComponentSetup(instance) {
+  const component = instance.type;
+
+  if (!component.render) {
+    instance.render = component.render;
+  }
+}
